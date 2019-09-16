fc1a0b0c75fefe992a6c3c8bcd6160e754df06b4
mackacavs <mackenziegdixon@yahoo.com>
2019-09-16 15:47:41 +0100

    Finished FizzBuzz Project

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,18 +1,26 @@
 describe("FizzBuzz", function () {
-    var fizzbuzz;
+  var fizzbuzz;
 
   beforeEach(function () {
     fizzbuzz = new FizzBuzz();
   });
 
   it("Should return 1 when passed 1", function () {
-    console.log("came in here");
     expect(fizzbuzz.checkNumber(1)).toEqual(1)
   })
 
   it("Should return Fizz when passed 3", function () {
-      console.log("came in here 2");
     expect(fizzbuzz.checkNumber(3)).toEqual('Fizz')
   })
+
+  it("Should return Buzz when passed 5", function () {
+    expect(fizzbuzz.checkNumber(5)).toEqual('Buzz')
+  })
+
+  it("Should return FizzBuzz when passed 15", function () {
+    expect(fizzbuzz.checkNumber(15)).toEqual('FizzBuzz')
+  })
+
+
 
 })
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,9 +2,13 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.checkNumber = function (number) {
-  if (number === 3){
+  if (number % 5 === 0 && number % 3 === 0) {
+    return "FizzBuzz";
+  } else if (number % 5 === 0) {
+    return "Buzz"
+  } else if (number % 3 === 0) {
     return "Fizz";
-  }else{
+  } else {
     return number
   }
 };
